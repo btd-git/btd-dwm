8df256f5de3a0547e0bbf74b96c367603f7b50d8
BTD <169353918+btd-git@users.noreply.github.com>
2024-05-19 12:51:34 +0530

    Create 1-packages.sh

---
+++ 1-packages.sh
@@ -0,0 +1,5 @@
+sudo pacman -Syy
+sudo pacman -S vim xorg xclip xwallpaper xcompmgr xdotool python-pywal sddm
+# IF YAY OR PARU OR ANY OTHER AUR MANAGER IS INSTALLED
+yay -Syy
+yay -S sddm-sugar-dark
