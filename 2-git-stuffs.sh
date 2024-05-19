8166e584154ae81b72c8caa97d2319fd2808e8b3
BTD <169353918+btd-git@users.noreply.github.com>
2024-05-19 13:31:08 +0530

    Create 2-git-stuffs.sh

---
+++ 2-git-stuffs.sh
@@ -0,0 +1,5 @@
+clear
+mkdir -p $HOME/Downloads/suckless-tools/
+git clone --depth=1 -C $HOME/Downloads/suckless-tools/ https://git.suckless.org/dwm
+git clone --depth=1 -C $HOME/Downloads/suckless-tools/ https://git.suckless.org/dmenu
+git clone --depth=1 -C $HOME/Downloads/suckless-tools/ https://git.suckless.org/st
